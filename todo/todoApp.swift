893108541c270ca9660e06d7fd8575e4cc2348c6
Himanish M <mandrekarh25@gmail.com>
2025-10-27 17:48:36 +0530

    Initial Commit

---
+++ todo/todoApp.swift
@@ -0,0 +1,32 @@
+//
+//  todoApp.swift
+//  todo
+//
+//  Created by Himanish M. on 27/10/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct todoApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
