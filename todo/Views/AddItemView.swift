22d8b43116d5cda44817787eaf2e652ac9a111a2
Himanish M. <himanish.mandrekar@coditas.com>
2025-10-30 17:58:40 +0530

    Refactor: Remove unnecessary comments.

--- todo/Views/AddItemView.swift
+++ todo/Views/AddItemView.swift
@@ -7,13 +7,10 @@ import SwiftUI
 import SwiftData
 
 struct AddItemView: View {
-    // Get the model context
     @Environment(\.modelContext) private var modelContext
     
-    // Get the environment's "dismiss" action
     @Environment(\.dismiss) private var dismiss
     
-    // State is now local to *this* view
     @State private var itemNameInput: String = ""
     @State private var itemContentInput: String = ""
 
@@ -22,7 +19,6 @@ struct AddItemView: View {
             VStack {
                 TextField("Title", text: $itemNameInput)
                     .padding(16)
-                    // Add some styling to see the field
                     .background(Color(.systemGray6))
                     .cornerRadius(10)
                 
@@ -31,7 +27,7 @@ struct AddItemView: View {
                     .background(Color(.systemGray6))
                     .cornerRadius(10)
                 
-                Spacer() // Pushes fields to the top
+                Spacer()
             }
             .padding()
             .navigationTitle("New Task")
@@ -48,8 +44,6 @@ struct AddItemView: View {
             }
         }
     }
-    
-    // The addItem logic now lives here
     private func addItem() {
         withAnimation {
             let newItem = Item(
